ae6d2143098219a6f2c412897008f429481dcd8f
Drachen_N01r <malfarizky23@gmail.com>
2023-08-15 15:50:43 +0700

    Update tampilan.php

--- app/Views/pages/tampilan.php
+++ app/Views/pages/tampilan.php
@@ -57,10 +57,6 @@
                 <p>Report</p>
               </a>
             </li>
-            <li class="nav-item">
-              <a href="pages/gallery.html" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
-                <p>Gallery</p>
               </a>
             </li>
           </ul>
